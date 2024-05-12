d897456016f566716da8c4d125ea0ce0bf8b46a8
Vadim Krupin <vadimkru02@gmail.com>
2024-05-12 05:51:31 +0300

    The dishes filter and urlencode

--- menu/views.py
+++ menu/views.py
@@ -8,14 +8,23 @@ from menu.models import Dishs
 def menu(request, slug):
 
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by  = request.GET.get('order_by', None)
 
-    
     if slug == 'all':
         dishs = Dishs.objects.all()
     else:    
         dishs = Dishs.objects.filter(category__slug=slug)
 
-    paginator = Paginator(dishs, 12)
+    if on_sale:
+        dishs = dishs.filter(discount__gt=0)
+        
+    if order_by and order_by != "default":
+        dishs = dishs.order_by(order_by)
+
+
+
+    paginator = Paginator(dishs, 4)
     current_page = paginator.page(int(page))    
 
     context = {
