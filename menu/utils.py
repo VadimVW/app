2d2dada04a1d0c69069944d38d5b6f070f1727fd
Vadim Krupin <vadimkru02@gmail.com>
2024-05-13 10:08:57 +0300

    The search has been improved

--- menu/utils.py
+++ menu/utils.py
@@ -7,20 +7,18 @@ from menu.models import Dishs
 
 
 def q_search(query):
-    
-    if query == '':
-        return Dishs.objects.all()
 
     # if query.isdigit() and len(query) <= 5:
     #     return Dishs.objects.filter(id=int(query))
-    
+
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-    
-    return Dishs.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
-    
 
-
+    return (
+        Dishs.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
     # keywords = [word for word in query.split() if len(word) > 2]
 
@@ -30,5 +28,4 @@ def q_search(query):
     #     q_objects |= Q(description__icontains=token)
     #     q_objects |= Q(name__icontains=token)
 
-
-    # return Dishs.objects.filter(q_objects)    
+    # return Dishs.objects.filter(q_objects)
