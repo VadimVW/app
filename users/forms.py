32d36f4ac4d53226e9684a03f6449c00221c0bb7
Vadim Krupin <vadimkru02@gmail.com>
2024-05-15 15:15:41 +0300

    Login form and controller

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,25 @@
+from logging import PlaceHolder
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField(
+        label= "Ім'я користувача",
+        widget=forms.TextInput(attrs={"autofocus": True,
+                                      'class': 'form-control',
+                                      'placeholder': "Введіть ім'я користувача"
+                                      })
+        )
+    password = forms.CharField(
+        label= "Пароль",
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+                                          'class': 'form-control',
+                                          'placeholder': "Введіть пароль"
+                                          }),
+    )
     class Meta:
         model = User
+        fields = ['username', 'password']
