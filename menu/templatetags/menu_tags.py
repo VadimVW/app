0cdd58b3c85173548af7c20a0b76a2b651eeabf2
Vadim Krupin <vadimkru02@gmail.com>
2024-05-16 14:46:56 +0300

    Added tamplatetag for sorted categories Added new fonts to css

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -1,5 +1,7 @@
+from os import name
 from django import template
 from django.utils.http import urlencode
+from django.db.models import Case, When
 
 from menu.models import Categories
 
@@ -9,6 +11,15 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag()
+def tag_sorted_categories():
+    return Categories.objects.annotate(
+    is_all_goods=Case(
+        When(name__contains="Всі товари", then=0),
+        default=1,
+    )
+).order_by('is_all_goods')
 
 @register.simple_tag(takes_context=True)
 def change_params(context, **kwargs):
